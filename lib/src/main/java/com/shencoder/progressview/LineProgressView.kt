0f6b36be41b7cc246babd8b501a85f4dae533f56
ShenBen <shenben19970221>
2021-05-18 18:38:47 +0800

    Initial commit

---
+++ lib/src/main/java/com/shencoder/progressview/LineProgressView.kt
@@ -0,0 +1,40 @@
+package com.shencoder.progressview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * 直线进度条自定义view
+ *
+ * @author  ShenBen
+ * @date    2021/05/17 10:18
+ * @email   714081644@qq.com
+ */
+class LineProgressView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private companion object {
+        private const val DEFAULT_MAX_PROGRESS = 100
+
+    }
+
+    init {
+        val typedArray =
+            context.obtainStyledAttributes(attrs, R.styleable.LineProgressView)
+
+        typedArray.recycle()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+
+    }
+}
